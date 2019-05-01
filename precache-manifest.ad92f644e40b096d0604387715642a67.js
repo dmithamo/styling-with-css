ef3b8dc720db2b2b144bc97498e867c549c36698
dmithamo1 <dennis.mithamo@andela.com>
2019-05-02 00:54:31 +0300

    Updates

---
+++ precache-manifest.ad92f644e40b096d0604387715642a67.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7daa70d966724275c9c0534899f435",
+    "url": "/styling-with-css/index.html"
+  },
+  {
+    "revision": "e53d19f738bf2d3b9e11",
+    "url": "/styling-with-css/static/js/2.e84d3edd.chunk.js"
+  },
+  {
+    "revision": "f896a52e6e8db1238d8d",
+    "url": "/styling-with-css/static/js/main.dc5f4a63.chunk.js"
+  },
+  {
+    "revision": "0fe9d9f1c16e4e580e92",
+    "url": "/styling-with-css/static/js/runtime~main.10ed9e38.js"
+  }
+]);
